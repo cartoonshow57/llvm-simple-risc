5e11ac51915658d656cb7ed706b108004a6d37c7
Lakshya Kharayat <cartoonshow57@gmail.com>
2025-07-25 13:39:30 +0000

    Cleaned the directory and started working on InstrInfo

--- llvm/lib/Target/SimpleRISC/TargetInfo/TargetInfoSimpleRISC.cpp
+++
@@ -1,9 +0,0 @@
-#include "llvm/MC/TargetRegistry.h"
-#include "TargetInfoSimpleRISC.h"
-
-using namespace llvm;
-
-extern "C" void LLVMInitializeSimpleRISCTargetInfo() {
-  RegisterTarget<Triple::simplerisc, /*HasJIT=*/false> X(
-      getTheSimpleRISCTarget(), "simplerisc", "SimpleRISC", "SimpleRISC");
-}
