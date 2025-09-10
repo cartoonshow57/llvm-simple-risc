837334151bcabbbb2df30df228a9a14e9ecf864b
Cartoonshow57 <cartoonshow57@gmail.com>
2025-09-10 13:11:49 +0530

    Fixed build error with this change

--- llvm/include/llvm/TargetParser/Triple.h
+++ llvm/include/llvm/TargetParser/Triple.h
@@ -78,6 +78,8 @@ public:
     amdgcn,         // AMDGCN: AMD GCN GPUs
     riscv32,        // RISC-V (32-bit): riscv32
     riscv64,        // RISC-V (64-bit): riscv64
+    riscv32be,
+    riscv64be,
     sparc,          // Sparc: sparc
     sparcv9,        // Sparcv9: Sparcv9
     sparcel,        // Sparc: (endianness = little). NB: 'Sparcle' is a CPU variant
